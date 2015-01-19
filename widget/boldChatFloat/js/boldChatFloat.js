6b99b859b540ab6d44a7634ff8f359d414b397f5
Ian Davis <ian.davis@oracle.com>
2015-01-19 14:17:11 +0000

    First commit

---
+++ widget/boldChatFloat/js/boldChatFloat.js
@@ -0,0 +1,48 @@
+/**
+ * @fileoverview BoldChat Floating Invite
+ * Option.
+ * @author ian.davis@oracle.com
+ */
+
+define(
+  //-------------------------------------------------------------------
+  // PACKAGE NAME
+  //-------------------------------------------------------------------
+  'boldChatFloat',
+
+  //-------------------------------------------------------------------
+  // DEPENDENCIES
+  //-------------------------------------------------------------------
+  ['knockout','ccLogger'],
+
+  //-------------------------------------------------------------------
+  // MODULE DEFINITION
+  //-------------------------------------------------------------------
+  function(ko, ccLogger) {
+
+    "use strict";
+
+    return {
+
+      onLoad: function(widgetModel) {
+
+        if(widgetModel.bcAccountID() === "") ccLogger.info("BoldChat Widget needs Account ID configured.");
+        if(widgetModel.bcWebsiteID() === "") ccLogger.info("BoldChat Widget needs Website ID configured.");
+
+        if(widgetModel.bcAccountID() !== "" && widgetModel.bcWebsiteID() !== "") {
+          window._bcvma = [];
+          _bcvma.push(["setAccountID", widgetModel.bcAccountID()]);
+          _bcvma.push(["setParameter", "WebsiteID", widgetModel.bcWebsiteID()]);
+          _bcvma.push(["setParameter", "InvitationID", widgetModel.bcInvitationID()]);
+          _bcvma.push(["setParameter", "VisitRef", widgetModel.user().id()]);
+          _bcvma.push(["setParameter", "VisitName", widgetModel.user().firstName() + " " + widgetModel.user().lastName()]);
+          _bcvma.push(["setParameter", "VisitEmail", widgetModel.user().emailAddress()]);
+          _bcvma.push(["addFloat", {type: "chat", id: widgetModel.bcButtonID()}]);
+          _bcvma.push(["pageViewed"]);
+          
+          require([document.location.protocol + '//vmss.boldchat.com/aid/' + widgetModel.bcAccountID() + '/bc.vms4/vms.js']);
+        }
+      }
+    };
+  }
+);
